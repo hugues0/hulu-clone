4120adfe9e682e6216035de9a2afa2eb25a87208
hugues0 <ntwari.hugues@gmail.com>
2021-08-31 00:17:28 +0200

    minor tweaks

--- components/Results.js
+++ components/Results.js
@@ -3,7 +3,7 @@ import Thumbnail from "./Thumbnail"
 
 const Results = ({results}) => {
     return (
-      <div className="px-5 m-10 sm:grid md:grid-cols-2 2xl:grid-cols-3 3xl:flex flex-wrap justify-items-center">
+      <div className="px-5 m-10 sm:grid md:grid-cols-2 2xl:grid-cols-3 3xl:flex flex-wrap align-center justify-center">
         {results.map((result) => (
           <Thumbnail key={result.id} result={result} />
         ))}
